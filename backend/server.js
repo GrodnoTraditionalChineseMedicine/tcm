50c2370825fe5191cd5275ac0e4721a2144a8980
Heenjiang <1207881522@qq.com>
2019-11-25 22:01:43 +0300

    roleManagement的所有Api

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,7 @@ const emailRouter = require('./src/router/email');
 const learntcmRouter = require('./src/router/learntcmApi');
 const pediatricCourseRouter = require('./src/router/pediatricCourse');
 const menusManagementRouter = require('./src/router/managementApi/menusManagement');
+const roleManagementRouter = require('./src/router/managementApi/roleManagement');
 
 //third-party middleware using
 app.use(express.json());
@@ -22,11 +23,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 //middleware using
-app.use('/api/manage',adminRouter);
+//前台系统Api
 app.use('/api/email', emailRouter);
 app.use('/api/learntcm',learntcmRouter);
 app.use('/api/pediatric', pediatricCourseRouter);
+//后台管理系统
+app.use('/api/manage',adminRouter);
 app.use('/api/manage/containers/content', menusManagementRouter);
+app.use('/api/manage/containers/roles', roleManagementRouter);
 
 app.listen(port, () => console.log(`app listening on port ${port}`));
 
