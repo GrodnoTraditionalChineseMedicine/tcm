9a1b109224d838bf546c78777d39826288468d9b
Lucas <1207881522@qq.com>
2020-02-21 10:49:33 +0300

    后台更新

--- backend/server.js
+++ backend/server.js
@@ -77,7 +77,7 @@ app.use(function(err, req, res, next) {
     res.render('error');
 });
 app.listen(3001,function () {
-   console.log('app runing on port 3001');
+   console.log('app running on port 3001');
 });
 module.exports = app;
 
